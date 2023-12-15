2af8b7097c464036e277fe053546aa4d4071a382
Pat0h <patrickngatia14@gmail.com>
2023-12-15 08:18:34 +0300

    cli.py update2

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,5 @@
 import click
-from lib.models import Policy, Policyholder, Claim
+from models import Policy, Policyholder, Claim
 from premium import calculate_premium
 from database import (
     create_policy,
@@ -17,36 +17,98 @@ from database import (
 def cli():
     pass
 
+# Policy-related commands
 @cli.command()
-def list_policies():
-    policies = []
-    click.echo('All policies:')
-    for policy in policies:
-        click.echo(f'- Policy ID: {policy.id}, Type: {policy.policy_type}, Start Date: {policy.start}, End Date: {policy.end}')
+@click.option('--policyholder-id', type=int, help='ID of the policyholder')
+@click.option('--start', type=str, help='Start date of the policy')
+@click.option('--end', type=str, help='End date of the policy')
+@click.option('--coverage', type=str, help='Coverage type')
+def create_policy(policyholder_id, start, end, coverage):
+    policyholder = get_policyholder_by_id(policyholder_id)
+    if policyholder:
+        premium = calculate_premium('health', coverage, policyholder.age)
+        new_policy = create_policy('health', start, end, premium, coverage, policyholder_id)
+        click.echo(f"Policy created with ID: {new_policy.id}")
+    else:
+        click.echo("Policyholder not found.")
 
 @cli.command()
-@click.argument('policy_id')
-def view_policy(policy_id):
+@click.argument('policy_id', type=int)
+def get_policy(policy_id):
     policy = get_policy_by_id(policy_id)
     if policy:
-        click.echo(f'Policy Details for ID {policy_id}:')
-        click.echo(f'- Type: {policy.policy_type}')
-        click.echo(f'- Start Date: {policy.start}')
-        click.echo(f'- End Date: {policy.end}')
-        # Add other attributes as needed
+        click.echo(f"Policy ID: {policy.id}, Start Date: {policy.start}, End Date: {policy.end}, Premium: {policy.premium}, Coverage: {policy.coverage}")
     else:
-        click.echo(f'Policy with ID {policy_id} not found.')
+        click.echo("Policy not found.")
 
 @cli.command()
-@click.argument('policy_type')
-@click.argument('start')
-@click.argument('end')
-@click.argument('policyholder_age', type=int)
-@click.argument('coverage')
-def create_policy(policy_type, start, end, policyholder_age, coverage):
-    premium = calculate_premium(policy_type, coverage, policyholder_age)
-    new_policy = create_policy(policy_type, start, end, premium, coverage)
-    click.echo(f'Created new policy - Type: {policy_type}, Start Date: {start}, End Date: {end}, Premium: {premium}')
+@click.argument('policy_id', type=int)
+def remove_policy(policy_id):
+    deleted = delete_policy(policy_id)
+    if deleted:
+        click.echo("Policy deleted successfully.")
+    else:
+        click.echo("Policy not found.")
+
+
+@cli.command()
+@click.option('--name', type=str, help='Name of the policyholder')
+@click.option('--email', type=str, help='Email of the policyholder')
+@click.option('--phone-number', type=str, help='Phone number of the policyholder')
+@click.option('--address', type=str, help='Address of the policyholder')
+def create_policyholder(name, email, phone_number, address):
+    new_policyholder = create_policyholder(name, email, phone_number, address)
+    click.echo(f"Policyholder created with ID: {new_policyholder.id}")
+
+@cli.command()
+@click.argument('policyholder_id', type=int)
+def get_policyholder(policyholder_id):
+    policyholder = get_policyholder_by_id(policyholder_id)
+    if policyholder:
+        click.echo(f"Policyholder ID: {policyholder.id}, Name: {policyholder.name}, Email: {policyholder.email}, Phone: {policyholder.phone_number}, Address: {policyholder.address}")
+    else:
+        click.echo("Policyholder not found.")
+
+@cli.command()
+@click.argument('policyholder_id', type=int)
+def remove_policyholder(policyholder_id):
+    deleted = delete_policyholder(policyholder_id)
+    if deleted:
+        click.echo("Policyholder deleted successfully.")
+    else:
+        click.echo("Policyholder not found.")
+
+
+@cli.command()
+@click.option('--policy-id', type=int, help='ID of the policy associated with the claim')
+@click.option('--claim-date', type=str, help='Date of the claim')
+@click.option('--claim-details', type=str, help='Details of the claim')
+@click.option('--status', type=str, help='Status of the claim')
+def create_claim(policy_id, claim_date, claim_details, status):
+    policy = get_policy_by_id(policy_id)
+    if policy:
+        new_claim = create_claim(policy_id, policy.policyholder_id, claim_date, claim_details, status)
+        click.echo(f"Claim created with ID: {new_claim.id}")
+    else:
+        click.echo("Policy not found.")
+
+@cli.command()
+@click.argument('claim_id', type=int)
+def get_claim(claim_id):
+    claim = get_claim_by_id(claim_id)
+    if claim:
+        click.echo(f"Claim ID: {claim.id}, Policy ID: {claim.policy_id}, Policyholder ID: {claim.policyholder_id}, Date: {claim.claim_date}, Details: {claim.claim_details}, Status: {claim.status}")
+    else:
+        click.echo("Claim not found.")
+
+@cli.command()
+@click.argument('claim_id', type=int)
+def remove_claim(claim_id):
+    deleted = delete_claim(claim_id)
+    if deleted:
+        click.echo("Claim deleted successfully.")
+    else:
+        click.echo("Claim not found.")
 
 if __name__ == '__main__':
     cli()
