ab814071e5d1538d70d1fdc80c9593dd76f9261d
Pat0h <patrickngatia14@gmail.com>
2023-12-15 05:17:37 +0300

    click

--- lib/cli.py
+++ lib/cli.py
@@ -1,17 +1,35 @@
 import click
 from models import Policy, Policyholder, Claim
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 def list_policies():
     click.echo('All policies')
 
-@cli.command()
-def view_policy(policy_id)
-    click.echo(f'Viewing policy with Id: {policy_id}')
 
 @cli.command()
-@click.argument('')
+def view_policy(policy_id):
+    click.echo(f'Viewing policy with Id: {policy_id}')
+
+
+@cli.command()
+@click.argument('policy_type')
+@click.argument('start')
+@click.argument('end')
+def create_policy(policy_type, start, end):
+    click.echo(f'Creating new policy- Type: {policy_type}, Start Date: {start}, End Date: {end}')
+
+
+@cli.command()
+@click.argument('policy_id')
+def delete_policy(policy_id):
+    click.echo(f'Deleting policy with Id: {policy_id}')
+
+
+if __name__ == '__main__':
+    cli()
