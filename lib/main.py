f8b1e148e33c5bdc75440dd55d1d4f0b756b7fc0
Pat0h <patrickngatia14@gmail.com>
2023-12-15 08:23:01 +0300

    cli.py update2

--- lib/main.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from lib.models import Policy, Policyholder, Claim
-
-engine = create_engine('sqlite:///insurance.db')
-Session = sessionmaker(bind=engine)
-session = Session()
